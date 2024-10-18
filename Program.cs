fddb3149dd512c3cb13e728cdd48863919d05340
KissAdam <kissa@kkszki.hu>
2024-10-18 10:46:05 +0200

    Ready to use

--- Program.cs
+++ Program.cs
@@ -8,8 +8,43 @@ namespace Doga2
 {
     internal class Program
     {
+
+        static void szamlalas()
+        {
+            int positiveSum = 0;
+            int negativeSum = 0;
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write($"Kérem adjon meg egy egész számot ({i + 1}/10): ");
+                string input = Console.ReadLine();
+
+                if (int.TryParse(input, out int number))
+                {
+                    if (number > 0)
+                    {
+                        positiveSum += number;
+                    }
+                    else if (number < 0)
+                    {
+                        negativeSum += number;
+                    }
+                    
+                }
+                else
+                {
+                    Console.WriteLine("Kérem, egy érvényes egész számot adjon meg.");
+                    i--;
+                }
+            }
+
+            Console.WriteLine($"A pozitív számok összege: {positiveSum}");
+            Console.WriteLine($"A negatív számok összege: {negativeSum}");
+        }
+
         static void Main(string[] args)
         {
+            szamlalas();
         }
     }
 }
